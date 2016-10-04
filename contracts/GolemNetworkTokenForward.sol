6ca2ead2dba3c0456805203cdc3ba81a1c199b37
Viggith <viggith@fewsquares.com>
2016-10-04 18:23:32 +0200

    Delete GolemNetworkTokenForward.sol

--- contracts/GolemNetworkTokenForward.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.1;
-
-import "ERC20TokenInterface.sol";
-
-
-contract GolemNetworkToken is ERC20TokenInterface {
-
-    function issueTokens(address _receiver, uint _numTokens) returns (bool);
-}
