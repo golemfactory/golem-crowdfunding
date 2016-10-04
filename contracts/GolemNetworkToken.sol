09a01a845f9e1cafeea9f48de21f7ea4ca11972c
Viggith <viggith@fewsquares.com>
2016-10-04 17:53:45 +0200

    Update GolemNetworkToken.sol

--- contracts/GolemNetworkToken.sol
+++ contracts/GolemNetworkToken.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.1;
 
 import "StandardToken.sol";
-import "GolemCrowdfundingForward.sol";
 
 
 contract GolemNetworkToken is StandardToken {
@@ -19,48 +18,30 @@ contract GolemNetworkToken is StandardToken {
     uint8 public constant decimals = 21; // FIXME: 1 ETH will be changed to 1000 GNT and 1 ETH has 18 decimal units
     string public constant symbol = "GNT";
 
-    GolemCrowdfunding constant golemCrowdfunding = GolemCrowdfunding({{GolemCrowdfunding}}); // TODO: 
+    address constant owner = {{address(GolemCrowdfunding contract)}}; // TODO: 
 
-    modifier noEther() {
-        if (msg.value > 0) {
+    modifier ownerOnly () {
+        if (msg.sender != owner) {
             throw;
         }
         _
     }
 
-    modifier crowdfundingContractOnly () {
-        if (msg.sender != address(golemCrowdfunding)) {
-            throw;
-        }
-        _
-    }
-
-    /// @notice Issues specified number of tokens on behalf of the crowdfunding contract.
+    /// @notice Issues specified number of tokens on behalf of the crowdfunding contract (owner).
     /// @param _receiver Address of the receiver.
     /// @param _numTokens Number of tokens to issue.
-    function issueTokens(address _receiver, uint _numTokens) external crowdfundingContractOnly returns (bool)
+    function issueTokens(address _receiver, uint _numTokens) external ownerOnly returns (bool)
     {
         if (_numTokens == 0) {
             return false;
         }
+        
         balances[_receiver] += _numTokens;
         totalSupply += _numTokens;        
+        
         Transfer(0, address(golemCrowdfunding), _numTokens);
         Transfer(address(golemCrowdfunding), _receiver, _numTokens);
+        
         return true;
-    }
-
-    function transfer(address _to, uint256 _value) noEther returns (bool)
-    {
-        return super.transfer(_to, _value);
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) noEther returns (bool)
-    {
-        return super.transferFrom(_from, _to, _value);
-    }
-
-    function approve(address _spender, uint256 _value) noEther returns (bool success) {
-        return super.approve(_spender, _value);
     }
 }
