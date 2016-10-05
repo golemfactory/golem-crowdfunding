d25ddbcb86f618bb22a23b70dad2a983b5e4efcd
Viggith <viggith@fewsquares.com>
2016-10-05 04:19:14 +0200

    GolemCrowdfunding contract made more readable
    configuration and crowdfunding status separated; minor naming changes introduced; superfluous functions removed; additional comments added; added clearly visible sections

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -140,53 +140,66 @@ contract GolemNetworkToken is StandardToken {
 
 contract GolemCrowdfunding {
 
-    // GNT contract 
-    GolemNetworkToken public golemNetworkToken;
- 
-    // Static config
-    uint256 constant ETH2GNTRate = 1000;
-    uint256 constant minCapInETH = 74074 ether
-    uint256 constant maxCapInETH = 740740 ether;
+    /*
+     *********************************** CONFIGURATION AND STATUS DATA STRUCTURES ***********************************  
+     */
+     
+    // Crowdfunding config
+    struct CrowdfundingConfig {
+        uint256 constant percentTokensForFounder = 18;
 
-    // Make sure that this address points to the multisig wallet
+        uint256 constant ETH2GNTRate = 1000;
+
+        uint256 constant minCapInETH = 74074 ether
+        uint256 constant maxCapInETH = 740740 ether;
+
+        uint256 crowdfundingStartBlock;
+        uint256 crowdfundingEndBlock;
+    }
+    
+    // Crowdfunding status
+    struct CrowdfundingStatus {
+        mapping (address => uint) fundersETHLedger;
+
+        uint256 balanceETH;
+        uint256 issuedGNT;
+
+        bool halted;
+        bool finalized;
+    }
+
+    /*
+     ******************************************* CROWDFUNDING VARIABLES ********************************************
+     */
+     
+    CrowdfundingConfig config;
+    CrowdfundingStatus status;
+
+    // Make sure that this address points to a multisig wallet - this is the founder wallet
     address owner;
- 
-    uint256 crowdfundingStartBlock;
-    uint256 crowdfundingEndBlock;
-    
-    // emergency
-    bool halted;
-    
-    // crowdfunding state
-    uint256 crowdfundingETHBalance;
-    uint256 issuedGNT;
-    mapping (address => uint) fundersETHLedger;
-    bool finalized;
 
-    // modifiers
-    
+    GolemNetworkToken  gnt;
+ 
+    /*
+     ************************************************** MODIFIERS **************************************************
+     */
+     
     modifier belowMaxCap() {
-        // Make sure that current message is below maximum cap
-        // Do not allow generating more than the cap.
-        // UI should known that and propose available number of tokens,
-        // but still it is a race condition.
-        // Alternatively, we can generate up the cap and return the left ether
-        // to the sender. But calling unknown addresses is a sequrity risk.
-        if (msg.value > (maxCapInETH - crowdfundingETHBalance)) {
+        if (msg.value > (config.maxCapInETH - status.balanceETH)) {
             throw;
         }
         _
     }
 
     modifier afterCrowdfunding() {
-        if (block.number <= crowdfundingEndBlock) {
+        if (block.number <= config.crowdfundingEndBlock) {
             throw;
         }
         _
     }
 
     modifier onlyIfMinGoalReached() {
-        if (crowdfundingETHBalance < minCapInETH) {
+        if (status.balanceETH < config.minCapInETH) {
             throw;
         }
         _
@@ -201,15 +214,19 @@ contract GolemCrowdfunding {
     }
 
     modifier notHalted() {
-        if (halted) {
+        if (status.halted) {
             throw;
         }
         _
     }
 
+    /*
+     ************************************************ EMERGENCY API ************************************************
+     */
+
     /// @notice Checks whether crowdfunding is in safe state`
     function checkIfIsInSafeState() constant private {
-        if (crowdfundingETHBalance > this.balance) {
+        if (status.balanceETH > this.balance) {
             throw;
         }
     }
@@ -217,7 +234,7 @@ contract GolemCrowdfunding {
     /// @notice fallback function called by anyone if and the contract is not in the safe state
     function emergencyFallback() external noEther returns (bool)
     {
-        if (crowdfundingETHBalance > this.balance) {
+        if (status.balanceETH > this.balance) {
             if (this.balance > 0 && !founder.send(this.balance)) {
                 throw;
             }
@@ -228,12 +245,12 @@ contract GolemCrowdfunding {
 
     // Emergency halt - in case something unexpected happens
     function haltCrowdfunding() external onlyOwner {
-        halted = true;
+        status.halted = true;
     }
     
     // Resume crowdfunding contract
     function resumeCrowdfunding() external onlyOwner {
-        halted = false;
+        status.halted = false;
     }
  
     /// @notice fallback function called if for some reason owner address has to be changed
@@ -241,116 +258,106 @@ contract GolemCrowdfunding {
         owner = _newOwner;
     }
 
+    /*
+     ********************************************** CREATION AND SETUP *********************************************
+     */
+
     function GolemCrowdfunding(uint256 _crowdfundingStartBlock, uint256 _crowdfundingEndBlock) {
         owner = msg.sender;
-        crowdfundingStartBlock = _crowdfundingStartBlock;
-        crowdfundingEndBlock = _crowdfundingEndBlock;
         
-        // FIXME: leave or comment out (by default all member variabels are initialized this way)
-        halted = false;
-        crowdfundingETHBalance = 0;
-        issuedGNT = 0;
-        finalized = false;
+        config.crowdfundingStartBlock = _crowdfundingStartBlock;
+        config.crowdfundingEndBlock = _crowdfundingEndBlock;
+        
+        status.balanceETH = 0;
+        status.issuedGNT = 0;
+        status.halted = false;
+        status.finalized = false;
     }
 
     /// @notice Setup function sets external contracts' addresses.
     /// @param golemNetworkTokenAddress Token address.
     function initialize(address _golemNetworkTokenAddress) external notHalted onlyOwner returns (bool)
     {
-        if (address(golemNetworkToken) == 0) {
-            golemNetworkToken = GolemNetworkToken(_golemNetworkTokenAddress);
+        if (address(gnt) == 0) {
+            gnt = GolemNetworkToken(_golemNetworkTokenAddress);
             return true;
         }
         return false;
     }
     
-    // Checks if minimum goal was reached
-    function minGoalReached() private constant returns (bool) {
-        return crowdfundingETHBalance >= minCapInETH;
-    }
+    /*
+     *********************************************** CROWDFUNDING API **********************************************
+     */
 
-    // If before fundingh period, return all eth from the current message to the owner
-    // If in the funding period, generate tokens for incoming ethers
+    // Crowdfunding entry point (this is the only function that should be called by funders)
+    // If before funding period, return all eth from the current message to the owner
+    // If during the funding period, generate tokens for incoming ethers
     // If after the funding period, conditionally handle investment return (if the minimum cap was not reached)
     function() external payable notHalted {
 
         // Make sure that this contract doesn't do anything before the crowdfunding start date
-        if (block.number < crowdfundingStartBlock) throw;
+        if (block.number < config.crowdfundingStartBlock) throw;
         
-        if (block.number <= crowdfundingEndBlock) {
-            handleCrowdfunding();
-        } else {
-            handlePostCrowdfunding();
+        // Crowdfunding ongoing
+        if (block.number <= config.crowdfundingEndBlock) {
+            var numTokens = msg.value * config.ETH2GNTRate;
+
+            if( numtokens > 0 ) {
+                if (!gnt.issueTokens(msg.sender, numTokens)) {
+                    throw;
+                }
+
+                // Update ETH ledger and track total ETH sent to this contract
+                status.fundersETHLedger[msg.sender] += msg.value;
+                status.balanceETH += msg.value;    
+                status.issuedGNT += numTokens;
+            }
+        } else { // After the crowdfunding
+            if (msg.value > 0) throw;
+
+            // Minimum cap not reached
+            if (status.balanceETH < config.minCapInETH) {
+                var funderETH = status.fundersETHLedger[msg.sender];
+                status.fundersETHLedger[msg.sender] = 0;
+                status.balanceETH -= funderETH;
+
+                if (funderETH > 0 && !msg.sender.send(funderETH)) {
+                    throw;
+                }
+            }
         }
 
         checkIfIsInSafeState();
     }
 
-    // Handles generation of tokens
-    function handleCrowdfunding() private belowMaxCap {
-        var numTokens = msg.value * ETH2GNTRate;
-
-        if( numtokens > 0 ) {
-            if (!golemNetworkToken.issueTokens(msg.sender, numTokens)) {
-                throw;
-            }
-
-            // Update ETH ledger and track total ETH sent to this contract
-            fundersETHLedger[msg.sender] += msg.value;
-            crowdfundingETHBalance += msg.value;    
-            issuedGNT += numTokens;
-        }
-    }
-    
-    // Return funder ETH
-    function refundFunder() private {    
-        var funderETH = fundersETHLedger[msg.sender];
-        fundersETHLedger[msg.sender] = 0;
-        crowdfundingETHBalance -= funderETH;
-
-        if (funderETH > 0 && !msg.sender.send(funderETH)) {
-            throw;
-        }
-    }
-
-    // Handles token refund in case minCapInETH was not reached
-    function handlePostCrowdfunding() private {
-        if (msg.value > 0) throw;
-
-        if (!minGoalReached()) {
-            refundFunder();
-        }
-    }
-
     // Finalize crowdfunding (send ETH to the founder and generate additional founder GNT)
     function finalze() external notHalted onlyOwner afterCrowdfunding onlyIfMinGoalReached {
-        if (!finalized) {
+        if (!status.finalized) {
             // Transfer funds to the owner (founder)
-            var totalFundedETH = crowdfundingETHBalance;
-            crowdfundingETHBalance = 0;
+            var totalFundedETH = status.balanceETH;
+            status.balanceETH = 0;
+
             if (totalFundedETH > 0 && !owner.send(totalFundedETH)) {
                 throw;
             }
-            
-            // Generate additional tokens for the Founder
-            var founderNumTokens = issuedGNT * percentTokensForFounder / (100 - percentTokensForFounder);
 
-            if (!golemNetworkToken.issueTokens(founder, founderNumTokens)) {
+            // Generate additional tokens for the Founder
+            var founderNumTokens = status.issuedGNT * config.percentTokensForFounder / (100 - config.percentTokensForFounder);
+
+            if (!gnt.issueTokens(owner, founderNumTokens)) {
                 throw;
             }
 
-            founderTokensIssued = true;
-            issuedGNT += founderNumTokens;
-
-            finalized = true;
+            status.issuedGNT += founderNumTokens;
+            status.finalized = true;
 
             checkIfIsInSafeState();
 
             // Cleanup. Remove all data not needed any more.
             // Also zero the founder address to indicate that funding has been
             // finalized.
-            crowdfundingStartBlock = 0;
-            crowdfundingEndBlock = 0;
+            config.crowdfundingStartBlock = 0;
+            config.crowdfundingEndBlock = 0;
         }
     }
 }
