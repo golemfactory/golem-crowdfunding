fbfa5c40b72bb0915c96247addf9029dba36587f
Paweł Bylica <chfast@gmail.com>
2016-10-07 23:29:29 +0200

    Remove ERC20 Token interface

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,50 +1,10 @@
 pragma solidity ^0.4.1;
 
-// Abstract contract for the full ERC 20 Token standard
-// https://github.com/ethereum/EIPs/issues/20
-
-contract ERC20TokenInterface {
-    /// total amount of tokens
-    function totalSupply() constant returns (uint256);
-
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance);
-
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success);
-
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success);
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    event Export(address indexed _from, address indexed _to, uint256 _value);
-}
-
 contract TokenImporter {
     function importTokens(address _from, uint256 _value);
 }
 
-contract GolemNetworkToken is ERC20TokenInterface {
+contract GolemNetworkToken {
     string public standard = 'Token 0.1'; // TODO: I think we should remove it.
 
     string public constant name = "Golem Network Token";
@@ -64,6 +24,10 @@ contract GolemNetworkToken is ERC20TokenInterface {
 
     address public importer;
     uint256 public totalExported;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Export(address indexed _from, address indexed _to, uint256 _value);
 
     function GolemNetworkToken(address _founder, uint256 _fundingStart,
                                uint256 _fundingEnd) {
