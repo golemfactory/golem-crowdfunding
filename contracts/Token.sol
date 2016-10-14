a21050050987a5fc6a7a70464873334af115c7af
Viggith <viggith@fewsquares.com>
2016-10-14 07:34:25 +0200

    Added developers, changed the default function
    1. Developers' GNT creation is explicit (for each developer) and takes place after the contract is finalized.
    2. Function() has to be called one more time to finalize developers' GNT creation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,7 +6,7 @@ contract MigrationAgent {
 
 contract GolemNetworkToken {
     string public constant name = "Golem Network Token";
-    uint8 public constant decimals = 10^18; // TODO: SET before THE CROWDSALE!
+    uint8 public constant decimals = 18; // TODO: SET before THE CROWDSALE!
     string public constant symbol = "GNT";
 
     // TODO: SET these PARAMS before THE CROWDSALE!
@@ -20,8 +20,33 @@ contract GolemNetworkToken {
     uint256 fundingStartBlock;
     uint256 fundingEndBlock;
  
-    address public founder;  
-    address public devAddress;  //Address 
+    address public golemAgent;  
+
+    // These addresses must be known before the contract is deployed
+    // TODO: SET before THE CROWDSALE!
+    // Invariant:
+    // dev0Percent + dev1Percent + dev2Percent + dev3Percent + dev4Percent + dev5Percent = 100
+    // dev0Percent > 0 && dev1Percent > 0 && dev2Percent > 0 && dev3Percent > 0 && dev4Percent > 0 && dev5Percent > 0
+    // FIXME: array based approach can be used instead, provided that it is safe to use this Solidity feature
+    uint256 private numCreatedTokensForDevelopers;
+    
+    address public dev0;
+    uint256 public dev0Percent;
+
+    address public dev1;
+    uint256 public dev1Percent;
+
+    address public dev2;
+    uint256 public dev2Percent;
+
+    address public dev3;
+    uint256 public dev3Percent;
+
+    address public dev4;
+    uint256 public dev4Percent;
+
+    address public dev5;
+    uint256 public dev5Percent;
     
     uint256 totalTokens;
     mapping (address => uint256) balances;
@@ -34,12 +59,14 @@ contract GolemNetworkToken {
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     event Migrate(address indexed _from, address indexed _to, uint256 _value);
 
-    function GolemNetworkToken(address _founder, address _devAddress, uint256 _fundingStartBlock,
+    function GolemNetworkToken(address _golemAgent, uint256 _fundingStartBlock,
                                uint256 _fundingEndBlock) {
-        founder = _founder;
-        devAddress = _devAddress;
+        golemAgent = _golemAgent;
         fundingStartBlock = _fundingStartBlock;
         fundingEndBlock = _fundingEndBlock;
+        
+        // MAX_UINT (uninitialized state)
+        numCreatedTokensForDevelopers = 2 ** 256 - 1;
     }
 
     // ERC20 Token Interface:
@@ -105,7 +132,7 @@ contract GolemNetworkToken {
     }
 
     function setMigrationAgent(address _agent) external {
-        if (msg.sender != founder) throw;
+        if (msg.sender != golemAgent) throw;
         if (migrationEnabled()) throw;  // Do not allow changing the importer.
         migrationAgent = _agent;
     }
@@ -126,6 +153,10 @@ contract GolemNetworkToken {
         return fundingEndBlock == 0;
     }
 
+    function developersEndowmentFinalized() constant returns (bool) {
+        return numCreatedTokensForDevelopers == 0;
+    }
+    
     // Are we in the funding period?
     function fundingOngoing() constant returns (bool) {
         if (fundingHasEnded())
@@ -143,70 +174,104 @@ contract GolemNetworkToken {
         return fundingMax - totalTokens;
     }
 
-    function changeFounder(address _newFounder) external {
+    function changeGolemAgent(address _newGolemAgent) external {
         // TODO: Sort function by importance.
-        if (msg.sender == founder)
-            founder = _newFounder;
+        if (msg.sender == golemAgent)
+            golemAgent = _newGolemAgent;
     }
 
     // If during the funding period, generate tokens for incoming ethers and finalize funding in case, cap was reached.
     // After the funding period - finalize funding
+    // FIXME: if we're going to leave the logic according to this design then I'd rather use enumeration to indicate the state of the contract (provided that it is implemented in a secure way in Solidity)
     function() payable external {
-        if (fundingFinalized()) throw;
+        if (developersEndowmentFinalized()) throw;
 
-        if (fundingHasEnded()) {
+        if (fundingFinalized()) {
             // Do not allow any eth transfer in this case
             if(msg.value > 0) throw;
-            
-            // Finalize funding in case the cap was not reached but the funding has ended
-            finalizeFunding();
+
+            finalizeDevelopersEndowment();
         }
         else {
-            // Only in funding period.
-            if (!fundingOngoing()) throw;
+            if (fundingHasEnded()) {
+                // Do not allow any eth transfer in this case
+                if(msg.value > 0) throw;
+                
+                // Finalize funding in case the cap was not reached but the funding has ended
+                finalizeFunding();
+            }
+            else {
+                // Only in funding period.
+                if (!fundingOngoing()) throw;
 
-            var numTokens = msg.value * tokenCreationRate;
-            if (numTokens == 0) throw;
+                var numTokens = msg.value * tokenCreationRate;
+                if (numTokens == 0) throw;
 
-            // Do not allow generating more than the cap.
-            // UI should known that and propose available number of tokens,
-            // but still it is a race condition.
-            // Alternatively, we can generate up the cap and return the left ether
-            // to the sender. But calling unknown addresses is a sequrity risk.
-            if (numTokens > numberOfTokensLeft()) throw;
+                // Do not allow generating more than the cap.
+                // UI should known that and propose available number of tokens,
+                // but still it is a race condition.
+                // Alternatively, we can generate up the cap and return the left ether
+                // to the sender. But calling unknown addresses is a sequrity risk.
+                if (numTokens > numberOfTokensLeft()) throw;
 
-            // Assigne new tokens to the sender
-            balances[msg.sender] += numTokens;
-            totalTokens += numTokens;
-            // Notify about the token generation with a transfer event from 0 address.
-            Transfer(0, msg.sender, numTokens);
-            
-            if (0 == numberOfTokensLeft()) {
-                // Cap reached - finalize funding
-                finalizeFunding();                
+                // Assigne new tokens to the sender
+                balances[msg.sender] += numTokens;
+                totalTokens += numTokens;
+                // Notify about the token generation with a transfer event from 0 address.
+                Transfer(0, msg.sender, numTokens);
+                
+                if (0 == numberOfTokensLeft()) {
+                    // Cap reached - finalize funding
+                    finalizeFunding();                
+                }
             }
         }
     }
 
+    // Transfers ETH to the golemAgent address
+    // Creates GNT for the golemAgent (representing the company)
+    // Allocates GNT for the developers (but the creation process is postponed)
     function finalizeFunding() private {
-        // Transfer ETH to the founder address
-        if (!founder.send(this.balance)) throw;
+        // Transfer ETH to the golemAgent address
+        if (!golemAgent.send(this.balance)) throw;
         
         // Generate additional tokens for the Founder and the developers.
         var additionalTokens = totalTokens * (percentTokensForFounder + percentTokensForDevelopers) / (100 - percentTokensForFounder - percentTokensForDevelopers);
         
-        var tokensForFounder   = additionalTokens * percentTokensForFounder / (percentTokensForFounder + percentTokensForDevelopers);
-        var tokensForDevelpers = additionalTokens - tokensForFounder;
+        var tokensForGolemAgent = additionalTokens * percentTokensForFounder / (percentTokensForFounder + percentTokensForDevelopers);
+        var tokensForDevelpers  = additionalTokens - tokensForGolemAgent;
  
-        balances[founder] += tokensForFounder;
-        balances[devAddress] += tokensForDevelpers;
+        balances[golemAgent] += tokensForFounder;
+        numCreatedTokensForDevelopers = tokensForDevelpers;
         
         totalTokens += additionalTokens;
 
         // Cleanup. Remove all data not needed any more.
-        // Also zero the founder address to indicate that funding has been
+        // Also zero the golemAgent address to indicate that funding has been
         // finalized.
         fundingStartBlock = 0;
         fundingEndBlock = 0;    
+    }
+    
+    // Sets GNT for each specified developer according to the specified GNT distribution 
+    function finalizeDevelopersEndowment() private {
+        if (numCreatedTokensForDevelopers == 2**256 -1) throw; //Impossible state!!!
+
+        var dev0Tokens = dev0Percent * numCreatedTokensForDevelopers / 100;
+        var dev1Tokens = dev1Percent * numCreatedTokensForDevelopers / 100;
+        var dev2Tokens = dev2Percent * numCreatedTokensForDevelopers / 100;
+        var dev3Tokens = dev3Percent * numCreatedTokensForDevelopers / 100;
+        var dev4Tokens = dev4Percent * numCreatedTokensForDevelopers / 100;
+        var dev5Tokens = numCreatedTokensForDevelopers - dev0Tokens - dev1Tokens - dev2Tokens - dev3Tokens - dev4Tokens;
+
+        balances[dev0] = dev0Tokens;
+        balances[dev1] = dev1Tokens;
+        balances[dev2] = dev2Tokens;
+        balances[dev3] = dev3Tokens;
+        balances[dev4] = dev4Tokens;
+        balances[dev5] = dev5Tokens;
+
+        // Indicate that this phase is also finished
+        numCreatedTokensForDevelopers = 0;
     }
 }
