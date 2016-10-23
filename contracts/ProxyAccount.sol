1c3867ab107d9919790be5d62567e3526f32922c
viggith <viggith@fewsquares.com>
2016-10-24 00:32:56 +0200

    proxy accounts example

---
+++ contracts/ProxyAccount.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.4.1;
+
+
+contract GolemNetworkToken {
+
+    function transfer(address _to, uint256 _value) returns (bool success);
+    function balanceOf(address _owner) constant returns (uint256 balance);
+
+    function migrate(uint256 _value);
+    function setMigrationAgent(address _agent);
+
+    function changeGolemFactory(address _golemFactory);
+}
+
+
+contract TimeLockedGNTProxyAccount {
+
+    address public owner;
+
+    uint256 public availableAfter;
+    address public gnt;
+
+
+    // Modifiers
+	
+    modifier ownerOnly {
+        if (msg.sender != owner) throw;
+        _;
+    }
+
+    modifier notLocked {
+    	if (now < availableAfter) throw;
+    	_;
+    }
+
+
+    // Creation and initialization
+
+    function TimeLockedGNTProxyAccount(uint256 _availableAter) {
+        owner = msg.sender;
+        availableAfter = _availableAter;
+    }
+
+    function setGNTContract(address _gnt) ownerOnly external {
+        gnt = _gnt;
+    }
+
+
+    // Token interface
+
+    function transfer(address _to, uint256 _value) notLocked ownerOnly returns (bool success) {
+        return GolemNetworkToken(gnt).transfer(_to, _value);
+    }
+    
+
+    // Migration interface
+
+    function migrate(uint256 _value) ownerOnly external {
+        GolemNetworkToken(gnt).migrate(_value);
+    }
+
+}
+
+
+contract TimeLockedGolemFactoryProxyAccount is TimeLockedGNTProxyAccount {
+
+    // Creation and initialization
+
+    function TimeLockedGolemFactoryProxyAccount(uint256 _availableAter) TimeLockedGNTProxyAccount(_availableAter) {
+    }
+    
+
+    // Golem Factory privileged API
+
+    function changeGolemFactory(address _golemFactory) ownerOnly external {
+        GolemNetworkToken(gnt).changeGolemFactory(_golemFactory);
+    }
+
+    // Migration interface
+
+    function setMigrationAgent(address _agent) ownerOnly external {
+        GolemNetworkToken(gnt).setMigrationAgent(_agent);
+    }
+
+}
