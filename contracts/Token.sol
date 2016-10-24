c2891d0b4196e40687d388a3ab168ba64aeadf1a
Paweł Bylica <chfast@gmail.com>
2016-10-24 12:29:36 +0200

    Cleanups

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,18 +9,18 @@ contract GolemNetworkToken {
     string public constant symbol = "GNT";
     uint8 public constant decimals = 18;  // 18 decimal places, the same as ETH.
 
-    // TODO: SET these PARAMS before THE CROWDFUNDING!
     uint256 constant percentTokensGolemFactory = 12;
     uint256 constant percentTokensDevelopers = 6;
     uint256 public constant tokenCreationRate = 1000;
 
-    // The funding cap in wei.
+    // The funding cap in weis.
     uint256 public constant tokenCreationCap = 820000 ether * tokenCreationRate;
-    uint256 public constant tokenCreationMin =  150000 ether * tokenCreationRate;
+    uint256 public constant tokenCreationMin = 150000 ether * tokenCreationRate;
 
     uint256 fundingStartBlock;
     uint256 fundingEndBlock;
 
+    // The flag indicates if the GNT contract is in "funding" mode.
     bool fundingMode = true;
 
     address public golemFactory;
@@ -48,7 +48,9 @@ contract GolemNetworkToken {
     address public constant dev5 = 0xde05;
     // uint256 public dev5Percent;  can be calculated as: 100 - dev0Percent - dev1Percent - dev2Percent - dev3Percent - dev4Percent
 
+    // The currect total token supply.
     uint256 totalTokens;
+
     mapping (address => uint256) balances;
 
     address public migrationAgent;
@@ -57,6 +59,7 @@ contract GolemNetworkToken {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Migrate(address indexed _from, address indexed _to, uint256 _value);
 
+    // Checks if in Funding Active state. Aborts transaction otherwise.
     modifier inFundingActive {
         if (!fundingMode) throw;
         // FundingActive: b ≥ Start and b ≤ End and t < Max
@@ -66,6 +69,7 @@ contract GolemNetworkToken {
         _;
     }
 
+    // Checks if in Funding Failure state. Aborts transaction otherwise.
     modifier inFundingFailure {
         if (!fundingMode) throw;
         // FundingFailure: b > End and t < Min
@@ -74,6 +78,7 @@ contract GolemNetworkToken {
         _;
     }
 
+    // Checks if in Funding Success state. Aborts transaction otherwise.
     modifier inFundingSuccess {
         if (!fundingMode) throw;
         // FundingSuccess: (b > End and t ≥ Min) or t ≥ Max
@@ -83,31 +88,35 @@ contract GolemNetworkToken {
         _;
     }
 
+    // Checks if in Operational state. Aborts transaction otherwise.
     modifier inOperational {
         if (fundingMode) throw;
         _;
     }
 
+    // Checks if in Operational Normal state. Aborts transaction otherwise.
     modifier inNormal {
         if (fundingMode) throw;
         if (migrationAgent != 0) throw;
         _;
     }
 
+    // Checks if in Operational Migration state. Aborts transaction otherwise.
     modifier inMigration {
         if (fundingMode) throw;
         if (migrationAgent == 0) throw;
         _;
     }
 
-    function GolemNetworkToken(address _golemFactory, uint256 _fundingStartBlock,
+    function GolemNetworkToken(address _golemFactory,
+                               uint256 _fundingStartBlock,
                                uint256 _fundingEndBlock) {
         golemFactory = _golemFactory;
         fundingStartBlock = _fundingStartBlock;
         fundingEndBlock = _fundingEndBlock;
     }
 
-    function transfer(address _to, uint256 _value) inOperational returns (bool success) {
+    function transfer(address _to, uint256 _value) inOperational returns (bool) {
         var senderBalance = balances[msg.sender];
         if (senderBalance >= _value && _value > 0) {
             senderBalance -= _value;
@@ -119,11 +128,11 @@ contract GolemNetworkToken {
         return false;
     }
 
-    function totalSupply() constant returns (uint256) {
+    function totalSupply() external constant returns (uint256) {
         return totalTokens;
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+    function balanceOf(address _owner) external constant returns (uint256) {
         return balances[_owner];
     }
 
@@ -213,8 +222,12 @@ contract GolemNetworkToken {
         if (!golemFactory.send(this.balance)) throw;
 
         // 2. Create GNT for the golemFactory (representing the company)
-        var numAdditionalTokens = totalTokens * (percentTokensGolemFactory + percentTokensDevelopers) / (100 - percentTokensGolemFactory - percentTokensDevelopers);
-        var numTokensForGolemAgent = numAdditionalTokens * percentTokensGolemFactory / (percentTokensGolemFactory + percentTokensDevelopers);
+        var numAdditionalTokens =
+            totalTokens * (percentTokensGolemFactory + percentTokensDevelopers) /
+            (100 - percentTokensGolemFactory - percentTokensDevelopers);
+        var numTokensForGolemAgent =
+            numAdditionalTokens * percentTokensGolemFactory /
+            (percentTokensGolemFactory + percentTokensDevelopers);
 
         balances[golemFactory] += numTokensForGolemAgent;
 
@@ -226,7 +239,8 @@ contract GolemNetworkToken {
         var dev2Tokens = dev2Percent * numTokensForDevelpers / 100;
         var dev3Tokens = dev3Percent * numTokensForDevelpers / 100;
         var dev4Tokens = dev4Percent * numTokensForDevelpers / 100;
-        var dev5Tokens = numTokensForDevelpers - dev0Tokens - dev1Tokens - dev2Tokens - dev3Tokens - dev4Tokens;
+        var dev5Tokens = numTokensForDevelpers - dev0Tokens - dev1Tokens -
+                         dev2Tokens - dev3Tokens - dev4Tokens;
 
         balances[dev0] += dev0Tokens;
         balances[dev1] += dev1Tokens;
