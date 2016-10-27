49015a0f3456588cc52b94a26c1048bdc7048ae6
Viggith <viggith@fewsquares.com>
2016-10-27 13:25:04 +0200

    contract generating proxy accounts

--- contracts/ProxyAccount.sol
+++ contracts/ProxyAccount.sol
@@ -23,8 +23,8 @@ contract TimeLockedGNTProxyAccount {
 
     // Creation and initialization
 
-    function TimeLockedGNTProxyAccount(uint256 _availableAfter) {
-        owner = msg.sender;
+    function TimeLockedGNTProxyAccount(address _owner, uint256 _availableAfter) {
+        owner = _owner;
         availableAfter = _availableAfter;
     }
 
@@ -56,7 +56,7 @@ contract TimeLockedGolemFactoryProxyAccount is TimeLockedGNTProxyAccount {
 
     // Creation and initialization
 
-    function TimeLockedGolemFactoryProxyAccount(uint256 _availableAfter) TimeLockedGNTProxyAccount(_availableAfter) {
+    function TimeLockedGolemFactoryProxyAccount(address _owner, uint256 _availableAfter) TimeLockedGNTProxyAccount(_owner, _availableAfter) {
     }
     
     // Modifiers
@@ -71,6 +71,7 @@ contract TimeLockedGolemFactoryProxyAccount is TimeLockedGNTProxyAccount {
     function changeGolemFactory(address _golemFactory) ownerOnly external {
         gnt.changeGolemFactory(_golemFactory);
     }
+    
     // Migration interface
 
     function setMigrationAgent(address _agent) ownerOnly external {
@@ -88,3 +89,84 @@ contract TimeLockedGolemFactoryProxyAccount is TimeLockedGNTProxyAccount {
         if (!owner.send(this.balance)) throw;
     }
 }
+
+contract ProxyAccountCreator {
+    
+    address owner;
+    uint256 availableAfter;
+    
+    uint256 numGeneratedProxies;
+    
+    uint256 private constant PROXIES_AT_ONCE = 4;
+    uint256 private constant NUM_DEVS = 23;
+    
+    bool finalized;
+    
+    event DevProxy(uint256 idx, address _addr);
+    event GolemFactoryProxy(address _addr);
+    
+    function ProxyAccountCreator(uint256 _availableAfter) {
+        owner = msg.sender;
+        availableAfter = _availableAfter;
+        numGeneratedProxies = 0;
+        finalized = false;
+    }
+
+    function min(uint256 a, uint256 b) private returns (uint256 result) {
+        if (a < b)
+            return a;
+            
+        return b;
+    }
+
+    function() {
+        if (msg.sender != owner) throw;
+        if (finalized) throw;
+        
+        address   golemFactory = 0xde23;
+        address[] devs = [
+            0xde00,
+            0xde01,
+            0xde02,
+            0xde03,
+            0xde04,
+            0xde05,
+            0xde06,
+            0xde07,
+            0xde08,
+            0xde09,
+            0xde10,
+            0xde11,
+            0xde12,
+            0xde13,
+            0xde14,
+            0xde15,
+            0xde16,
+            0xde17,
+            0xde18,
+            0xde19,
+            0xde20,
+            0xde21,
+            0xde22
+        ];   
+    
+        var bound = min(numGeneratedProxies + PROXIES_AT_ONCE, devs.length);
+        
+        for (uint256 i = numGeneratedProxies; i < bound; ++i) {
+            var devAddr = devs[i];
+            var proxyContract = new TimeLockedGNTProxyAccount(devAddr, availableAfter);
+            
+            numGeneratedProxies++;
+        
+            DevProxy(i, address(proxyContract));
+        }
+        
+        if(numGeneratedProxies == devs.length && !finalized) {
+            var golemFactoryProxyContract = new TimeLockedGolemFactoryProxyAccount(golemFactoryAddr, availableAfter);
+
+            finalized = true;
+            numGeneratedProxies++;
+
+            GolemFactoryProxy(address(golemFactoryProxyContract));
+        }
+    }
