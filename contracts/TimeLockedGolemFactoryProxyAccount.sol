64a869681f63cbf74f04710f7b02d025a092656c
Paul Peregud <paulperegud@gmail.com>
2016-11-03 16:08:44 +0100

    split proxy contracts file

---
+++ contracts/TimeLockedGolemFactoryProxyAccount.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.4;
+
+import "./Token.sol";
+
+contract TimeLockedGolemFactoryProxyAccount is TimeLockedGNTProxyAccount {
+
+    // Creation and initialization
+
+    function TimeLockedGolemFactoryProxyAccount(uint256 _availableAfter) TimeLockedGNTProxyAccount(_availableAfter) {
+    }
+
+    // Modifiers
+
+    modifier gntOnly {
+        if (msg.sender != address(gnt)) throw;
+        _;
+    }
+
+    // Golem Factory privileged API
+
+    function setMigrationMaster(address _migrationMaster) ownerOnly external {
+        gnt.setMigrationMaster(_migrationMaster);
+    }
+    // Migration interface
+
+    function setMigrationAgent(address _agent) ownerOnly external {
+        gnt.setMigrationAgent(_agent);
+    }
+
+    // Default function - allow transfers from the GNT contract only
+
+    function() gntOnly payable {
+    }
+
+    // Withdraw - transfer ETH to to the Golem Factory
+
+    function withdraw() ownerOnly {
+        if (!owner.send(this.balance)) throw;
+    }
+}
